f867c57687a1ee1b78d6ed30d22ec75bea2f6016
Unknown <by.darklancer@gmail.com>
2018-06-26 02:03:45 -0400

    feat: vista del administrador

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tarea, TipoTarea, EstadoTarea
 
 # Register your models here.
+admin.site.register(Tarea)
+admin.site.register(EstadoTarea)
+admin.site.register(TipoTarea)
